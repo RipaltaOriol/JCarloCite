187913bd386b28a6ef87e16ffccd61605830c709
Oriol Ripalta Maso <oriolripalta@hotmail.com>
2024-02-29 09:53:50 -0500

    build: setup heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn jcarlo.wsgi:application --log-file -
--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-i20no24)50ps6t32_fygu8n-)$y@^2az$&ekpa67fe$p6t0ty2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -123,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
