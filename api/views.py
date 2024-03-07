c8c55f810463c602a9215f7d9091f3c70ecb888d
Oriol Ripalta Maso <oriolripalta@hotmail.com>
2024-03-07 11:53:40 +0100

    feat: add file upload API endpoint

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 import json
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def api_home(request, *args, **kwargs):
     body = request.body #.decode('utf-8')
@@ -11,3 +12,14 @@ def api_home(request, *args, **kwargs):
         print(e)
     print(data)
     return JsonResponse({"message": "first api endping reached"})
+
+# TODO: delete this for production
+@csrf_exempt
+def upload_file(request, *args, **kwargs):
+    if request.method == "POST":
+        file = request.FILES['file_name']
+        # handle the file logic [Nico]
+        # ...
+        # ...
+        return JsonResponse({"message": "its a post"})
+    return JsonResponse({"message": "Wrong path..."})
